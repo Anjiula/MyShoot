b498b44f6dcc675c14130642567f3e8cb7bff43d
b_anhr <18772106903@163.com>
2018-01-15 18:22:48 +0800

    Signed-off-by: b_anhr <18772106903@163.com>

--- 周报/dwxb/java学习/java大内/src/day02/HomeWork.java
+++ 周报/dwxb/java学习/java大内/src/day02/HomeWork.java
@@ -1,39 +1,67 @@
 package day02;
 
+/**
+ * 作业一： 
+ * 判断输入的数字是小数还是整数
+ * 若是小数转换乘以5后输出		（有保留2位小数  ,两种方法）
+ * 若是整数转换乘以10后输出
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 小结： 保留2位小数
+ * DecimalFormat df = new DecimalFormat("#.00");
+ * df.format(f)
+ * 
+ * String.format("%.2f", f)
+ * 
+ * 返回的都是字符串
+ */
+import java.text.DecimalFormat;
+
+import com.sun.xml.internal.bind.v2.model.core.Adapter;
+
 public class HomeWork {
 
 	public static void main(String[] args) {
-		String string = "120";
+		
+		//write a string of number and decimal point only
+		String string = "10363.54";
 		
 		/**
-		 * 判断小数的正则
+		 * judging the regularity of the decimal
 		 */
-		String regex = "^[1-9]\\d*\\.\\d*|0\\.\\d*[1-9]\\d*$";
+		String regex = "^[-]?[1-9]\\d*\\.\\d*|[-][0-9]?\\d*\\.\\d*$";
 		
 		/**
-		 * 判断整数的正则
+		 * judging the regularity of the integer
 		 */
 		String regex2 = "^-?[1-9]\\d*$";
 		
-//		if (HomeWork.matchString(string, regex2)) {
-//			//小数
-//			double d = Double.parseDouble(string);
-//			System.out.println("小数" + d);
-//		}
-		if (HomeWork.matchString(string, regex2)) {
-			//整数
+		if (HomeWork.matchString(string, regex)) {
+			//decimal
+			double d = Double.parseDouble(string);
+			//方法一   
+			System.out.println("this number is decimals : " + d + "\n" + "five multiple result : " + HomeWork.transFormToDouble(d * 5));
+			System.out.println();
+			//方法二   
+			System.out.println("this number is decimals : " + d + "\n" + "five multiple result : " + String.format("%.2f", d * 5));
+		}else if (HomeWork.matchString(string, regex2)) {
+			//integer
 			int i = Integer.parseInt(string);
-			System.out.println("整数" + i);
+			System.out.println("this number is integer : " + i + "\n" + "ten multiple result : " + i * 10);
+		}else {
+			System.out.println("that is not a number");
 		}
 		
-
 	}
 	
 	/**
-	 * 
-	 * @param string	需要判断的字符串
-	 * @param regex		正则表达式
-	 * @return			0非   1是
+	 *  
+	 * @param string	string to be judged
+	 * @param regex		regular
+	 * @return			0no    1yes
 	 */
 	public static boolean matchString(String string, String regex) {
 		if (string.matches(regex)) {
@@ -41,4 +69,19 @@ public class HomeWork {
 		}
 		return false;
 	}
+	
+	/**
+	 * keep two bits after decimal
+	 * double to double
+	 * use decimalFmormat
+	 * @return
+	 */
+	public static double transFormToDouble(double num) {
+		DecimalFormat decimalFormat = new DecimalFormat("#.00");					
+		return Double.parseDouble(decimalFormat.format(num));
+	}
 }
+
+
+
+
